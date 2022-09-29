443e81b3719acfbac8c46317d7063c6be04fc71e
Lamnq <lamnq@smartosc.com>
2022-09-29 15:21:51 +0700

    x

--- Skinet/Infrastructure/Data/SpecificationEvaluator.cs
+++ Skinet/Infrastructure/Data/SpecificationEvaluator.cs
@@ -9,15 +9,15 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Data
 {
-    public class SpecificationEvaluator<TEntity> where TEntity : BaseEntity
+    public class SpecificationEvaluator<TEntity> where TEntity : Nurse
     {
         public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> inputQuery, ISpecification<TEntity> spec)
         {
             var query = inputQuery;
 
-            if(spec.Criteria != null)
+            if (spec.Criteria != null)
             {
-                query = query.Where(spec.Criteria); //p => p.ProductId == id
+                query = query.Where(spec.Criteria); 
             }
             query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
 
