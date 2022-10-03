f5bb05b9af78feb0249e0cd1e62b3afbec8562ba
minhnhitb <nguyennhuminh1999tb@gmail.com>
2022-10-03 17:56:41 +0700

    Add-Doctor-Table, Interface, Repository, Controller, View(Create, Index)

---
+++ Skinet/Infrastructure/Migrations/20221003102150_Add-Doctor-Table.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class AddDoctorTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Doctors",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "VARCHAR(255)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "VARCHAR(255)", nullable: false),
+                    Gender = table.Column<int>(type: "int", nullable: false),
+                    Birthday = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Department = table.Column<string>(type: "VARCHAR(255)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Doctors", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Doctors");
+        }
+    }
+}
---
+++ Skinet/Web/Controllers/DoctorController.cs
@@ -0,0 +1,41 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Web.Controllers
+{
+    public class DoctorController : Controller
+    {
+        private readonly IDoctorRepository _doctorRepository;
+        public DoctorController(IDoctorRepository doctorRepository)
+        {
+            _doctorRepository = doctorRepository;
+        }       
+        public async Task<IActionResult> Index()
+        {
+            var model = await _doctorRepository.GetDoctor();
+            return View(model);
+
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Doctor model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                await _doctorRepository.Create(model);
+                return RedirectToAction("Index");
+            }
+            return View(model);
+
+
+        }
+    }
+}
