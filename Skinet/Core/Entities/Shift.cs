864a234ff2094c6f8cc56c075c62767335988784
tran anh <fdfs.tran.anh@gmail.com>
2022-10-05 14:37:31 +0700

    edit table shift, workshift

--- Skinet/Core/Entities/Shift.cs
+++ Skinet/Core/Entities/Shift.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Core.Entities
 {
-    public class Shift : BaseEntity
+    public class Shift
     {
         public Guid IdShift { get; set; }
+        [Column(TypeName = "NVARCHAR(30)")]
         public string? ShiftName { get; set; }
         public string? Time { get; set; }
         public ICollection<WorkShift> WorkShift { get; set; }
--- Skinet/Core/Entities/Workshift.cs
+++ Skinet/Core/Entities/Workshift.cs
@@ -1,23 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Core.Entities
 {
-    public class WorkShift : BaseEntity
+    public class WorkShift
     {
-        public Guid IdDoctor { get; set; }
+        public Guid IdWork { get; set; }
         public Guid IdShift { get; set; }
-
+        [Column(TypeName = "NVARCHAR(50)")]
+        public string DoctorName { get; set; }
         [DataType(DataType.Date)]
         public DateTime? Date { get; set; }
-
+        [Column(TypeName = "NVARCHAR(20)")]
         public string? Status { get; set; }
-
         public Shift? Shift { get; set; }
-        public Doctor? Doctor { get; set; }
     }
 }
