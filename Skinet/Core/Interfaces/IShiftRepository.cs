80a94583acb3cda4b4306bc357d38bf26128663d
tran anh <fdfs.tran.anh@gmail.com>
2022-10-06 11:12:00 +0700

    no message

---
+++ Skinet/Core/Interfaces/IShiftRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Core.Interfaces
+{
+    public interface IShiftRepository
+    {
+        Task<Shift> Create(Shift request);
+
+        Task<Shift> Update(Shift request);
+
+        Task<Shift> Delete(Guid IdShift);
+        Task<Shift> GetById(Guid IdShift);
+    }
+}
---
+++ Skinet/Core/Interfaces/IWorkShiftRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Core.Interfaces
+{
+    public interface IWorkShiftRepository
+    {
+        Task<WorkShift> Create(WorkShift request);
+
+        Task<WorkShift> Update(WorkShift request);
+
+        Task<WorkShift> Delete(Guid IdWork);
+        Task<WorkShift> GetById(Guid IdWork);
+    }
+}
