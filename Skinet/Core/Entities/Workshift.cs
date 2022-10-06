9afecaaed37682cf721d168427cb8f94d77a0bf0
tran anh <fdfs.tran.anh@gmail.com>
2022-10-06 17:06:41 +0700

    no message

--- Skinet/Core/Entities/Workshift.cs
+++ Skinet/Core/Entities/Workshift.cs
@@ -4,12 +4,6 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Core.Entities
@@ -17,7 +11,7 @@ namespace Core.Entities
     public class WorkShift
     {
         public Guid IdWork { get; set; }
-        public int IdShift { get; set; }
+        public Guid IdShift { get; set; }
         public Guid Id { get; set; }
         [DataType(DataType.Date)]
         public DateTime? Date { get; set; }
