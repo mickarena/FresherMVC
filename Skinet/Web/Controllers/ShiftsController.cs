1cc8fa7aa3618905c00e6c07c24ed07727779226
tran anh <fdfs.tran.anh@gmail.com>
2022-10-07 07:27:10 +0700

    no message

---
+++ Skinet/Web/Controllers/ShiftsController.cs
@@ -0,0 +1,60 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Web.Controllers
+{
+    public class ShiftController : Controller
+    {
+        private readonly IShiftRepository _shiftRepository;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ShiftController(IShiftRepository shiftRepository, IWebHostEnvironment webHostEnvironment)
+        {
+            _shiftRepository = shiftRepository;
+            _webHostEnvironment = webHostEnvironment;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var model = await _shiftRepository.GetShift();
+            return View(model);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var shift = await _shiftRepository.GetById(id);
+            return View(shift);
+        }
+
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var shift = await _shiftRepository.GetById(id);
+            return View(shift);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Remove(Guid id)
+        {
+            if (ModelState.IsValid)
+            {
+                await _shiftRepository.Delete(id);
+                return RedirectToAction("Index");
+            }
+            return View(id);
+        }
+
+        public async Task<IActionResult> Search(string searchName)
+        {
+            var result = await _shiftRepository.Search(searchName);
+            return View("Index", result);
+        }
+    }
+}
