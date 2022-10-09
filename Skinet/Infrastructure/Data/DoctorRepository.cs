c1c2efc83c002c638a57567c3ea7a64f11e97fce
DESKTOP-JVEKVQT\Azend <fdfs.tran.anh@gmail.com>
2022-10-09 20:55:27 +0700

    no message

---
+++ Skinet/Infrastructure/Data/DoctorRepository.cs
@@ -0,0 +1,55 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class DoctorRepository : IDoctorRepository
+    {
+        private readonly StoreContext _context;
+        public DoctorRepository(StoreContext context)
+        {
+            _context = context;
+        }
+        public async Task<Doctor> Create(Doctor doctor)
+        {
+            await _context.Doctors.AddAsync(doctor);
+            await _context.SaveChangesAsync();
+            return doctor;
+        }
+
+        public async Task Delete(Guid id)
+        {
+            var doctorToDelete = await _context.Doctors.FindAsync(id);
+            _context.Doctors.Remove(doctorToDelete);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Doctor>> GetDoctor()
+        {
+            return await _context.Doctors.ToListAsync();
+        }
+
+        public async Task<Doctor> GetById(Guid id)
+        {
+            return await _context.Doctors.FirstOrDefaultAsync(x => x.Id == id);
+
+        }
+
+        public async Task Update(Doctor doctor)
+        {
+            _context.Entry(doctor).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public List<Doctor> GetType()
+        {
+            return _context.Doctors.ToList();
+        }
+    }
+}
