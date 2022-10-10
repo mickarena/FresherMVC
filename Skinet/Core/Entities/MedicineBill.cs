d13f42e8268fe1e8c9cf7c310d72f339a84735f8
minhtk <minhtk@smartosc.com>
2022-10-10 14:32:57 +0700

    no message

--- Skinet/Core/Entities/MedicineBill.cs
+++ Skinet/Core/Entities/MedicineBill.cs
@@ -1,18 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Core.Entities
 {
     public class MedicineBill : BaseEntity
     {
         [Required]
+        [DisplayName("Doctor ID")]
         public Guid DoctorID { get; set; }
 
+        [DisplayName("Day created")]
         public DateTime DateCreate { get; set; }
 
         [Required]
+        [DisplayName("Paying status")]
         public bool PayStatus { get; set; }
 
         public IEnumerable<MedicineBillInfo>? MedicineBillInfos { get; set; }
     }
 }
-
