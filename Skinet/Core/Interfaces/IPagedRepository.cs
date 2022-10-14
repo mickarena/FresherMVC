96b3daff3ef51ba8996ab4c8cd4c2a7637f2cf03
minhtk <minhtk@smartosc.com>
2022-10-14 18:00:08 +0700

    Update page repository

--- Skinet/Core/Interfaces/IPagedRepository.cs
+++ Skinet/Core/Interfaces/IPagedRepository.cs
@@ -1,16 +1,7 @@
-﻿using Core.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Core.Interfaces
+﻿namespace Core.Interfaces
 {
     public interface IPagedRepository<T> where T : List<T>
     {
-        public void PaginatedList(List<T> items, int count, int pageIndex, int pageSize);
-
-        public Task<DtoPagination<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize);
+        public List<T> PaginatedList(List<T> items, int count, int pageIndex, int pageSize);
     }
 }
---
+++ Skinet/Infrastructure/Data/PageRepository.cs
@@ -0,0 +1,31 @@
+﻿using Core;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class PageRepository<T> : IPagedRepository<T> where T : List<T>
+    {
+        private DtoPagination<T> dto;
+
+        public PageRepository()
+        {
+            dto = new DtoPagination<T>();
+        }
+
+        public List<T> PaginatedList(List<T> items, int pageIndex)
+        {
+            dto.PageIndex = pageIndex;
+            var count = items.Count;
+            var pageSize = 3;
+            var paginationItem = items.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
+            dto.TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            return paginationItem;
+        }
+    }
+}
