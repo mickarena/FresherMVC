1bf7866f7139a6392bfef77ce416ff3fc3226eb7
minhtk <minhtk@smartosc.com>
2022-10-16 20:43:38 +0700

    page repository

--- Skinet/Core/Interfaces/IPagedRepository.cs
+++ Skinet/Core/Interfaces/IPagedRepository.cs
@@ -2,6 +2,6 @@
 {
     public interface IPagedRepository<T> where T : List<T>
     {
-        public List<T> PaginatedList(List<T> items, int count, int pageIndex, int pageSize);
+        public List<T> PaginatedList(List<T> items, int pageIndex);
     }
 }
