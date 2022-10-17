bf3bad7312053c70916f8a185e6596b76bdc12ad
minhtk <minhtk@smartosc.com>
2022-10-17 17:04:01 +0700

    update doctor model (this commit is uploaded by Minhnn fresher-7000 in minhtk fresher--20102002 in this PC)

--- Skinet/Core/Entities/Doctor.cs
+++ Skinet/Core/Entities/Doctor.cs
@@ -7,19 +7,31 @@ namespace Core.Entities
     public class Doctor : BaseEntity
     {
         [Required]
+        [MaxLength(255)]
         public string Name { get; set; }
+
         [Required]
+        [MaxLength(100)]
         public string Phone { get; set; }
+
         [Required]
+        [MaxLength(255)]
         public string Address { get; set; }
+
         public Gender Gender { get; set; }
         public DateTime Birthday { get; set; }
+
         [Required]
         public string Department { get; set; }
+
+        [Required]
+        [MaxLength(100)]
         public string Image { get; set; }
+
         [NotMapped]
         [Display(Name = "Upload File")]
         public IFormFile ImageFile { get; set; }
+
         public ICollection<WorkShift> WorkShift { get; set; }
     }
 }
