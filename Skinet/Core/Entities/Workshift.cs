a41e196095cc028873406df206a930ac6f95c3bc
fdfs.trananh@gmail.com <114552828+AnhTT241099@users.noreply.github.com>
2022-10-19 10:47:07 +0700

    edit model workshift, storecontext

--- Skinet/Core/Entities/Workshift.cs
+++ Skinet/Core/Entities/Workshift.cs
@@ -1,12 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Core.Entities
 {
     public class WorkShift : BaseEntity
     {
         [Required(ErrorMessage = "You haven't selected a shift yet")]
+        [ForeignKey("Shift")]
         public Guid IdShift { get; set; }
         [Required(ErrorMessage = "You haven't chosen a doctor yet")]
+        [ForeignKey("Doctor")]
         public Guid IdDoctor { get; set; }
         [DataType(DataType.Date)]
         [Required(ErrorMessage = "You haven't chosen a working day")]
