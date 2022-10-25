29d7afceabe1473992fc7bfa88989a41e55dc252
fdfs.trananh@gmail.com <114552828+AnhTT241099@users.noreply.github.com>
2022-10-25 15:09:04 +0700

    update entity shift: string --> datetime hh:mm

--- Skinet/Core/Entities/Shift.cs
+++ Skinet/Core/Entities/Shift.cs
@@ -11,17 +11,13 @@ namespace Core.Entities
         [Required(ErrorMessage = "The name of the shift cannot be empty")]
         public string? ShiftName { get; set; }
 
-        [Column(TypeName = "VARCHAR(5)")]
-        [StringLength(5, MinimumLength = 5)]
-        [RegularExpression(@"^([0-1][0-9]|2[0-3]):[0-5][0-9]$", ErrorMessage = "Input is incorrect, ex: 12:59")]
+        [DisplayFormat(DataFormatString = "{0:hh:mm}", ApplyFormatInEditMode = true)]
         [Required(ErrorMessage = "You have not selected a work start time")]
-        public string? StartTime { get; set; }
+        public DateTime? StartTime { get; set; }
 
-        [Column(TypeName = "VARCHAR(5)")]
-        [StringLength(5, MinimumLength = 5)]
-        [RegularExpression(@"^([0-1][0-9]|2[0-3]):[0-5][0-9]$", ErrorMessage = "Input is incorrect, ex: 12:59")]
+        [DisplayFormat(DataFormatString = "{0:hh:mm}", ApplyFormatInEditMode = true)]
         [Required(ErrorMessage = "You have not selected a work end time")]
-        public string? EndTime { get; set; }
+        public DateTime? EndTime { get; set; }
         public ICollection<WorkShift>? WorkShift { get; set; }
     }
 }
